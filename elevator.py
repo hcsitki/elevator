b869eb8ecd74a71fd4a5b6a10aeb66252ea61d4c
Hunter Sitki <hunter.sitki@gmail.com>
2024-06-10 10:04:35 -0600

    Added a main function to run the elevator through

--- elevator.py
+++ elevator.py
@@ -6,29 +6,27 @@ ASSUMPTIONS:
     - 1-indexing user and elevator floors for clarity
 '''
 
-
 '''
 Function simulating the elevator running and being available at all times.
 '''
-def run_elevator(user, elev):
-    print(f"\nYou walk up to the elevator in the {NUM_FLOORS} story building. Currently, the elevator is on floor {elev}")
+def run_elevator(user, elev, NUM_FLOORS):
+    # Simulate another person using the elevator and leaving it on a random floor
+    elev = random.randint(1, NUM_FLOORS)
+    print(f"Someone has left the elevator on floor {elev}")
 
     # Keep the elevator ready to be called
     while True:
         user_input = input("Type 'c' to call the elevator, or 'q' if you would like to quit the program\n")
         if user_input.lower() == 'c' or user_input.lower() == 'call':   # Checking the user makes the correct call and accepts valid edge case inputs
-            call_elevator(user, elev)
+            call_elevator(user, elev, NUM_FLOORS)
             break
         elif user_input.lower() == 'q' or user_input.lower() == 'quit': # Option to quit the program
+            print("Thank you for using the elevator!")
             exit()
         else:   # Only accept the call or quit as valid input
-            print("\nInvalid input. Please type 'c' to call the elevator, or 'q' if you would like to quit the program\n")
-    
-    # Simulate another person using the elevator and leaving it on a random floor
-    elev = random.randint(1, NUM_FLOORS)
+            print("Invalid input. Please type 'c' to call the elevator, or 'q' if you would like to quit the program\n")
 
-def call_elevator(user, elev):
-
+def call_elevator(user, elev, NUM_FLOORS):
     while True:
         sel_floor = input(f"The elevator has been called from floor {elev} to you on floor {user}. Which floor would you like to go to? (1 - {NUM_FLOORS})\n")
         try:
@@ -40,18 +38,20 @@ def call_elevator(user, elev):
         except ValueError:
             print("Invalid input. Please select a floor number")
 
-    print(f"The elevator comes to floor {user}, you get in and it takes you to floor {sel_floor}")
-
+    print(f"The elevator comes to floor {user}, you get in and it takes you to floor {sel_floor}. Have a nice time here!\n")
     elev = sel_floor
     user = sel_floor
 
     # Put the elevator back into running
-    run_elevator(user, elev)
+    run_elevator(user, elev, NUM_FLOORS)
 
+def main():
+    user = 1
+    MAX_FLOORS = 100
+    NUM_FLOORS = random.randint(2, MAX_FLOORS)
+    elev = random.randint(1, NUM_FLOORS)
+    print(f"\nYou walk up to the elevator in the {NUM_FLOORS} story building and go to the elevator")
 
-user = 1
-MAX_FLOORS = 100
-NUM_FLOORS = random.randint(2, MAX_FLOORS)
-elev = random.randint(1, NUM_FLOORS)
+    run_elevator(user, elev, NUM_FLOORS)
 
-run_elevator(user, elev)
+main()
